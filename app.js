97077cfc3a61d4f45d6aa9f70141439a2b224648
Harsh Prajapati <harsh@Harshs-MacBook-Air.local>
2022-08-14 22:28:37 +0530

    change local port to 3000

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ app.get("/:customList", function (req, res) {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 8000;
+    port = 3000;
 }
 
 app.listen(port, function () {
