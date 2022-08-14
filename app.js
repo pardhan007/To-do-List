cf5c6a9776ed31048848e3a322275370bb132751
Harsh Prajapati <harsh@Harshs-MacBook-Air.local>
2022-08-14 23:39:47 +0530

    edited port in app,js

--- app.js
+++ app.js
@@ -144,11 +144,7 @@ app.get("/:customList", function (req, res) {
 });
 
 
-let port = process.env.PORT;
-if (port === null || port === "") {
-    port = 3000;
-}
-
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("Server started succesfully");
 });          
