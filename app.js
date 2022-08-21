ccc8e74221c657bda29a00c0026fcb0b522b2840
Harsh Prajapati <harsh@Harshs-MacBook-Air.local>
2022-08-21 16:05:01 +0530

    added url in google api & callback

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/todolist",
+    callbackURL: "https://radiant-brushlands-90419.herokuapp.com/auth/google/todolist",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
