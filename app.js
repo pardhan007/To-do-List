eb4304c348e89db4b0cbedff2ae0485863939822
Harsh Prajapati <harsh@Harshs-MacBook-Air.local>
2022-08-21 16:00:10 +0530

    setup login & signup page & only google login

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-//jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/data.js");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
-const _ = require("lodash");
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const date = require(__dirname + "/data.js");
 
 
 const app = express();
@@ -16,133 +20,238 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-harsh:harsh123@cluster0.6asgh9f.mongodb.net/todolistDB", { useNewUrlParser: true });
+app.use(session({
+    secret: "Our little Secret.",
+    resave: false,
+    saveUninitialized: false,
+}));
 
-const itemsSchema = {
+app.use(passport.initialize()); // initalize session
+app.use(passport.session()); // use passport to manage session
+
+mongoose.connect("mongodb+srv://admin-harsh:harsh123@cluster0.6asgh9f.mongodb.net/todoModified", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/todoModified", { useNewUrlParser: true });
+
+
+const itemsSchema = new mongoose.Schema({
     name: String
-};
+});
 
 const Item = mongoose.model("Item", itemsSchema);
 
-// const item1 = new Item({
-//     name: "Welcome to TodoList!"
-// });
-// const item2 = new Item({
-//     name: "Hit + to Add more"
-// });
-// const item3 = new Item({
-//     name: "<- to delete item!"
-// });
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    userItems: [itemsSchema]
+});
 
-// const defaultItems = [item1, item2, item3];
 
-const defaultItems = [];
+userSchema.plugin(passportLocalMongoose); // hash & salt
+userSchema.plugin(findOrCreate);
 
-const listSchema = {
-    name: String,
-    items: [itemsSchema]
-}
+const User = mongoose.model("User", userSchema);
 
-const List = mongoose.model("List", listSchema);
+passport.use(User.createStrategy());
 
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/todolist",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        // console.log(profile);
+
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+const day = date.getDate();
 app.get("/", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.redirect("/todolist");
+    }
+    else {
+        res.render("login");
+    }
+
+});
+
+app.get("/auth/google",
+    passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/google/todolist",
+    passport.authenticate('google', { failureRedirect: "/" }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect('/todolist');
+    });
+
+app.get("/signup", function (req, res) {
+    res.render("signup");
+});
+
+
+app.get("/todolist", function (req, res) {
 
     let day = date.getDate();
-    Item.find({}, function (err, foundItems) {
-        // if (foundItems.length === 0) {
-        //     Item.insertMany(defaultItems, function (err) {
-        //         if (err) {
-        //             console.log(err);
+    if (req.isAuthenticated()) {
 
-        //         }
-        //         else {
-        //             console.log("Successfully Saved Default Items!");
-        //         }
-        //     })
-        // }
-        res.render('list', { listTitle: "Today", newListItems: foundItems });
+        User.findById(req.user.id, function (err, foundUser) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                if (foundUser) {
+                    res.render("list", { listTitle: day, newListItems: foundUser.userItems });
+                }
+            }
+        });
+    }
+    else {
+        res.redirect("/");
+    }
+});
+
+
+
+
+app.post("/login", function (req, res) {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+            res.redirect("/login");
+        }
+        else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/todolist");
+            });
+            res.send("Wrong username or Password");
+        }
+    })
+
+});
+
+
+app.post("/register", function (req, res) {
+
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            // console.log("Hello");
+            console.log(err);
+            res.redirect("/signup");
+        }
+        else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/todolist");
+
+            });
+        }
+    });
+});
+
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect("/");
+        }
     });
 
 });
 
-app.post("/", function (req, res) {
-
+app.post("/todolist", function (req, res) {
     let item = req.body.itemName;
-    const listName = req.body.list;
-
-    const newItem = new Item({
+    const item1 = new Item({
         name: item
     });
-
-    if (listName === "Today") {
-        newItem.save();
-        res.redirect("/");
-
-    }
-    else {
-        List.findOne({ name: listName }, function (err, foundList) {
-            foundList.items.push(newItem);
-            foundList.save();
-            res.redirect("/" + listName);
-        });
-    }
-
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (foundUser) {
+                foundUser.userItems.push(item1);
+                foundUser.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        res.redirect("/todolist");
+                    }
+                });
+            }
+        }
+    });
 });
 
 app.post("/delete", function (req, res) {
-    // console.log(req.body.checkBox);
     const itemId = req.body.checkBox;
-    const listName = req.body.listName;
-    if (listName === "Today") {
-        Item.findByIdAndRemove(itemId, function (err) {
-            if (err) {
-                consile.log(err);
-            }
-            else {
-                // console.log("Successfully Deleted item!");
-            }
-        });
-        res.redirect("/");
-    }
-    else {
-
-        List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: itemId } } }, function (err, foundList) {
-            if (!err) {
-                res.redirect("/" + listName);
-            }
-
-        });
-
-    }
-});
-
-app.get("/:customList", function (req, res) {
-
-    // const customListName = _.lowerCase(req.params.customListName);
-    const customListName = _.capitalize(req.params.customList);
-
-    List.findOne({ name: customListName }, function (err, foundList) {
-        if (!err) {
-            if (!foundList) {
-                //create a new list
-                const list = new List({
-                    name: customListName,
-                    items: defaultItems
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (foundUser) {
+                const itemArr = foundUser.userItems;
+                User.findOneAndUpdate({ _id: req.user.id }, { $pull: { userItems: { _id: itemId } } }, function (err, foundList) {
+                    if (err) {
+                        consile.log(err);
+                    }
+                    else {
+                        // console.log("Successfully Deleted item!");
+                    }
                 });
 
-                list.save();
-                res.redirect("/" + customListName);
+                res.redirect("/todolist");
             }
-            else {
-                // show existinnh list
-                res.render('list', { listTitle: foundList.name, newListItems: foundList.items });
-            }
-
         }
-
     });
 });
 
+// app.get("/:customList", function (req, res) {
+//     // const customListName = _.lowerCase(req.params.customListName);
+//     const customListName = _.capitalize(req.params.customList);
+//     List.findOne({ name: customListName }, function (err, foundList) {
+//         if (!err) {
+//             if (!foundList) {
+//                 //create a new list
+//                 const list = new List({
+//                     name: customListName,
+//                     items: defaultItems
+//                 });
+//                 list.save();
+//                 res.redirect("/" + customListName);
+//             }
+//             else {
+//                 // show existinnh list
+//                 res.render('list', { listTitle: foundList.name, newListItems: foundList.items });
+//             }
+//         }
+//     });
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
